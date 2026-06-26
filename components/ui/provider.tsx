2f7dadebbdc15e39a274c629a548f8078a11f6c7
Oleg_Volostnykh <olegbasketball15@gmail.com>
2026-06-26 22:16:04 +0200

    feat(redux): set up redux for state management

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,16 +1,23 @@
 "use client"
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { Provider as ReduxProvider } from "react-redux"
 import type { ReactNode } from "react"
+import { useState } from "react"
+import { makeStore } from "@/lib/store"
 
 type ProviderProps = {
   children: ReactNode
 }
 
 export function Provider({ children }: ProviderProps) {
+  const [store] = useState(makeStore)
+
   return (
-    <ChakraProvider value={defaultSystem}>
-      {children}
-    </ChakraProvider>
+    <ReduxProvider store={store}>
+      <ChakraProvider value={defaultSystem}>
+        {children}
+      </ChakraProvider>
+    </ReduxProvider>
   )
 }
